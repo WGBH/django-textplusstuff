096640398e7a719ff614319b4d00110d11c86da8
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2016-03-29 09:24:04 -0400

    Swapping in standard libs importlib

--- textplusstuff/registry.py
+++ textplusstuff/registry.py
@@ -434,7 +434,7 @@ def findstuff():
     This is a near 1-to-1 copy of how django's admin application registers
     models.
     """
-    from django.utils.importlib import import_module
+    from importlib import import_module
     from django.utils.module_loading import module_has_submodule
 
     for app in settings.INSTALLED_APPS:
