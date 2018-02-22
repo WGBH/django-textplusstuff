9ced8ec598487e76f2c5c95c0fcf96cc46d4d24d
Jay Thompson <jay_thompson@wgbh.org>
2018-02-22 09:08:21 -0600

    Update migrations to work in newer versions of Django

--- textplusstuff/migrations/0001_initial.py
+++ textplusstuff/migrations/0001_initial.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
-from __future__ import unicode_literals
 
-from django.db import models, migrations
-import textplusstuff.fields
 from django.conf import settings
+from django.db import models, migrations
+import django.db.models.deletion
+import textplusstuff.fields
 
 
 class Migration(migrations.Migration):
 
+    initial = True
+
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('contenttypes', '0001_initial'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
 
     operations = [
@@ -24,13 +26,12 @@ class Migration(migrations.Migration):
                 ('date_modified', models.DateTimeField(help_text='The date this draft was updated.', verbose_name='Date Modified', auto_now=True)),
                 ('content', textplusstuff.fields.TextPlusStuffField(verbose_name='Content', blank=True)),
                 ('content_ported', models.BooleanField(default=False, help_text='Signifies whether or not this draft has been used to populate a TextPlusStuffField on another model.', verbose_name='Content Ported')),
-                ('user', models.ForeignKey(verbose_name='User', to=settings.AUTH_USER_MODEL, help_text='The user who created this draft.')),
+                ('user', models.ForeignKey(help_text='The user who created this draft.', null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='User')),
             ],
             options={
                 'verbose_name': 'Text Plus Stuff Draft',
                 'verbose_name_plural': 'Text Plus Stuff Drafts',
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='TextPlusStuffLink',
@@ -39,12 +40,11 @@ class Migration(migrations.Migration):
                 ('parent_object_id', models.PositiveIntegerField()),
                 ('object_id', models.CharField(max_length=25)),
                 ('field', models.CharField(max_length=50, verbose_name='Field')),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
-                ('parent_content_type', models.ForeignKey(related_name='textplusstufflink_parent_link', to='contenttypes.ContentType')),
+                ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
+                ('parent_content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='textplusstufflink_parent_link', to='contenttypes.ContentType')),
             ],
             options={
                 'verbose_name': 'Text Plus Stuff Links',
             },
-            bases=(models.Model,),
         ),
     ]
